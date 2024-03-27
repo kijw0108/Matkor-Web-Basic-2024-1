624877b416c39ac16c37658915caa24711064571
kijw0108 <89176573+kijw0108@users.noreply.github.com>
2024-03-27 21:43:38 +0900

    Update code

--- week2/prac/front/src/App.js
+++ week2/prac/front/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [num, setNum] = useState(0);
+
+  const sentID = () => {
+  }
+
+	return (
+		<div className="App">
+			{num}
+			<button onClick={() => setNum(num + 1)}>Click me</button>
+		</div>
+	);
 }
 
 export default App;
+
